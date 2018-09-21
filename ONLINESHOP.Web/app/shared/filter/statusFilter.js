cf38d5b2bf5f9ab5d2e0b9c1a74e707e178f12b6
tranngocthi <thictk1.09c050tk@gmail.com>
2018-09-21 12:01:24 +0700

    bai63

---
+++ ONLINESHOP.Web/app/shared/filter/statusFilter.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="\Assets/plugins/angular/angular.js" />
+
+(function (app) {
+    app.filter('statusFilter', statusFilter);
+
+    function statusFilter() {
+
+
+        return function (input) {
+
+            if (input)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    }
+
+
+})(angular.module('onlineshop.common'));
